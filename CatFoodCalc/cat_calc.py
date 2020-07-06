05a0e59ec3ec8fd77dc7c95959c201944fb0238d
hardpenguin <hardpenguin13@gmail.com>
2020-07-06 21:13:48 +0200

    commented code should be deleted or uncommented; moved preset values into presets.py; Cat.kcal_per_kg is a new dict that replaces two separate values; Cat.calculate_der() is more readable now; Cat.get_kcal_per_kg_by_activity() is now more readable, uses a dict by utilizing keys from presets.py; Cat.get_age_modifier() is now an empty prototype

--- CatFoodCalc/cat_calc.py
+++ CatFoodCalc/cat_calc.py
@@ -1,25 +1,12 @@
-class Cat:
-    # age = {
-    #     1: "kitten under 4 mo",
-    #     2: "kitten under 9 mo",
-    #     3: "kitten under 12 mo",
-    #     4: "adult cat",
-    #     5: "pregnant cat",
-    #     6: "lactating cat (under 3 kittens)",
-    #     7: "lactating cat (3+ kittens)",
-    #     8: "senior cat"
-    # }
-    # activity = {
-    #     1: "neutered, inactive and indoor cats",
-    #     2: "active and outdoor cats"
-    # }
+import presets
 
-    kcal_per_kg_indoor = [52, 75]
-    kcal_per_kg_outdoor = [100]
+class Cat(object):
 
     def __init__(self, weight, age, activity):
-        self.kcal_per_kg_outoor = Cat.kcal_per_kg_outdoor
-        self.kcal_per_kg_indoor = Cat.kcal_per_kg_indoor
+        self.kcal_per_kg = {
+                "outdoor": presets.kcal_per_kg["outdoor"],
+                "indoor": presents.kcal_per_kg["indoor"]
+            }
         self.weight = weight
         self.age = age
         self.activity = activity
@@ -27,17 +14,22 @@ class Cat:
         self.der = self.calculate_der()
 
     def calculate_der(self):
-        return [round(self.der_formula_by_weight * self.get_kcal_per_kg_by_activity()[0], 2),
-                round(self.der_formula_by_weight * self.get_kcal_per_kg_by_activity()[1], 2)]
+        der = self.der_formula_by_weight * self.get_kcal_per_kg_by_activity()
+        der["min"] = round(der["min"], 2)
+        der["max"] = round(der["max"], 2)
+
+        return(der)
 
     def get_kcal_per_kg_by_activity(self):
-        if self.activity == 2:
-            kcal_per_kg_min = self.kcal_per_kg_outoor[0]
-            kcal_per_kg_max = self.kcal_per_kg_outoor[0]
-        else:
-            kcal_per_kg_min = self.kcal_per_kg_indoor[0]
-            kcal_per_kg_max = self.kcal_per_kg_indoor[1]
-        return [kcal_per_kg_min, kcal_per_kg_max]
+        kcal_per_kg = {}
 
-    # def get_age_modifier(self):
-    #     # TODO
+        levels_by_activity = self.kcal_per_kg[self.activity]
+
+        kcal_per_kg["min"] = levels_by_activity["min"]
+        kcal_per_kg["max"] = levels_by_activity["max"]
+
+        return(kcal_per_kg)
+
+    def get_age_modifier(self):
+        # TODO
+        pass
---
+++ CatFoodCalc/presets.py
@@ -0,0 +1,27 @@
+age = {
+    1: "kitten under 4 mo",
+    2: "kitten under 9 mo",
+    3: "kitten under 12 mo",
+    4: "adult cat",
+    5: "pregnant cat",
+    6: "lactating cat (under 3 kittens)",
+    7: "lactating cat (3+ kittens)",
+    8: "senior cat"
+}
+
+activity = {
+    "indoor": "neutered, inactive and indoor cats",
+    "outdoor": "active and outdoor cats"
+}
+
+kcal_per_kg = {
+    "indoor": {
+        "min": 52,
+        "max": 75
+    },
+
+    "outdoor": {
+        "min": 100,
+        "max": 100
+    }
+}
