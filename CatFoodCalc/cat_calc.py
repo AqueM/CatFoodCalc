82efdcaf5a928cc863197ea78b85260de2c84e4d
Dorota Roskowska <dorota.roskowska@gmail.com>
2020-07-06 20:12:03 +0200

    added calculation by age, new test data, new Cat() variables, reformatted data, added sources for values

--- CatFoodCalc/cat_calc.py
+++ CatFoodCalc/cat_calc.py
@@ -1,43 +1,70 @@
-class Cat:
-    # age = {
-    #     1: "kitten under 4 mo",
-    #     2: "kitten under 9 mo",
-    #     3: "kitten under 12 mo",
-    #     4: "adult cat",
-    #     5: "pregnant cat",
-    #     6: "lactating cat (under 3 kittens)",
-    #     7: "lactating cat (3+ kittens)",
-    #     8: "senior cat"
-    # }
-    # activity = {
-    #     1: "neutered, inactive and indoor cats",
-    #     2: "active and outdoor cats"
-    # }
+import statistics
 
-    kcal_per_kg_indoor = [52, 75]
-    kcal_per_kg_outdoor = [100]
+
+class Cat:
+    # all nutritional values are as detailed in
+    # FEDIAF Nutritional Guidelines (2019) PDF,
+    # section 7.2.4. "Practical recommendations for daily energy intake
+    # by dogs and cats in different physiological states", subsection 7.2.4.2. Cats and table VII-9
+    # http://www.fediaf.org/images/FEDIAF_Nutritional_Guidelines_2019_Update_030519.pdf
+    # http://www.fediaf.org/self-regulation/nutrition.html
+
+    age_labels = {
+        0: "kitten under 4 mo",
+        1: "kitten under 9 mo",
+        2: "kitten under 12 mo",
+        3: "adult cat",
+        4: "pregnant cat",
+        5: "lactating cat (under 3 kittens)",
+        6: "lactating cat (3+ kittens)"
+    }
+    activity_labels = {
+        0: "neutered / inactive / indoor cats",
+        1: "active / outdoor cats"
+    }
+
+    kcal_by_activity = [
+        [52, 75],
+        [100, 100]
+    ]
+
+    age_modifier = [
+        [2, 2.5],
+        [1.75, 2],
+        [1.5, 1.5],
+        [1, 1],
+        [1.4, 1.4],
+        [1.2, 1.2],
+        [1.7, 1.7]
+    ]
 
     def __init__(self, weight, age, activity):
-        self.kcal_per_kg_outoor = Cat.kcal_per_kg_outdoor
-        self.kcal_per_kg_indoor = Cat.kcal_per_kg_indoor
         self.weight = weight
         self.age = age
         self.activity = activity
-        self.der_formula_by_weight = self.weight * 0.67
+        self.age_modif = self.get_age_modifier()
+        # RER - Resting Energy Requirement (by weight only)
+        self.rer_formula_by_weight = self.weight * 0.67
+        # DER - Daily Energy Requirement (by weight and activity)
         self.der = self.calculate_der()
+        self.der_avg = round(statistics.mean(self.der), 0)
+        # MER - Metabolic Energy Requirement (total, by weight, activity and age)
+        self.mer = self.calculate_mer()
+        self.mer_avg = round(statistics.mean(self.mer), 0)
 
     def calculate_der(self):
-        return [round(self.der_formula_by_weight * self.get_kcal_per_kg_by_activity()[0], 2),
-                round(self.der_formula_by_weight * self.get_kcal_per_kg_by_activity()[1], 2)]
+        return [round(self.rer_formula_by_weight * self.get_kcal_per_kg_by_activity()[0], 2),
+                round(self.rer_formula_by_weight * self.get_kcal_per_kg_by_activity()[1], 2)]
 
     def get_kcal_per_kg_by_activity(self):
-        if self.activity == 2:
-            kcal_per_kg_min = self.kcal_per_kg_outoor[0]
-            kcal_per_kg_max = self.kcal_per_kg_outoor[0]
-        else:
-            kcal_per_kg_min = self.kcal_per_kg_indoor[0]
-            kcal_per_kg_max = self.kcal_per_kg_indoor[1]
+        kcal_per_kg_min = self.kcal_by_activity[self.activity][0]
+        kcal_per_kg_max = self.kcal_by_activity[self.activity][1]
         return [kcal_per_kg_min, kcal_per_kg_max]
 
-    # def get_age_modifier(self):
-    #     # TODO
+    def get_age_modifier(self):
+        return self.age_modifier[self.age]
+
+    def calculate_mer(self):
+        mer_min = self.der[0] * self.age_modif[0]
+        mer_max = self.der[1] * self.age_modif[1]
+        return [mer_min, mer_max]
--- CatFoodCalc/test/test_cat.py
+++ CatFoodCalc/test/test_cat.py
@@ -1,20 +1,21 @@
 import unittest
-from unittest_data_provider import data_provider
 import CatFoodCalc.cat_calc as cat_calc
 
 
 class TestCatCalculations(unittest.TestCase):
     # weight, age, activity
-    data = [[4, 2, 1], [6, 2, 2]]
+    data = [[4, 3, 0], [6, 4, 1], [2, 0, 1]]
     cats_data = {}
-    # kcal per kg min, kcal per kg max, kcal per day min, kcal per day max
-    results = [[52, 75, 139.36, 201], [100, 100, 402, 402]]
+    # kcal per kg min, kcal per kg max, der min, der max, mer min, mer max
+    results = [[52, 75, 139.36, 201, 139.36, 201], [100, 100, 402, 402, 562.8, 562.8], [100, 100, 134, 134, 268, 335]]
     cats_results = {}
 
     methods_to_test = ["cat_calc.Cat.get_kcal_per_kg_by_activity(tested_cat)[0]",
                        "cat_calc.Cat.get_kcal_per_kg_by_activity(tested_cat)[1]",
                        "cat_calc.Cat.calculate_der(tested_cat)[0]",
-                       "cat_calc.Cat.calculate_der(tested_cat)[1]"]
+                       "cat_calc.Cat.calculate_der(tested_cat)[1]",
+                       "cat_calc.Cat.calculate_mer(tested_cat)[0]",
+                       "cat_calc.Cat.calculate_mer(tested_cat)[1]"]
 
     def construct_cats_with_results(self):
         for x in range(len(self.data)):
