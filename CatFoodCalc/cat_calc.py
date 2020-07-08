54c274c43713d444e2fb4389966ed67716bae2ac
Dorota Roskowska <dorota.roskowska@gmail.com>
2020-07-08 19:56:02 +0200

    corrections as per @HardPenguin 's suggestions, refactored calculations, moved nutritional data and labels out to a separate file. Refactored test_cat.py to more gracefully handle list of methods to test

--- CatFoodCalc/cat_calc.py
+++ CatFoodCalc/cat_calc.py
@@ -1,70 +1,46 @@
+from CatFoodCalc import nutritional_values
 import statistics
 
 
-class Cat:
-    # all nutritional values are as detailed in
-    # FEDIAF Nutritional Guidelines (2019) PDF,
-    # section 7.2.4. "Practical recommendations for daily energy intake
-    # by dogs and cats in different physiological states", subsection 7.2.4.2. Cats and table VII-9
-    # http://www.fediaf.org/images/FEDIAF_Nutritional_Guidelines_2019_Update_030519.pdf
-    # http://www.fediaf.org/self-regulation/nutrition.html
-
-    age_labels = {
-        0: "kitten under 4 mo",
-        1: "kitten under 9 mo",
-        2: "kitten under 12 mo",
-        3: "adult cat",
-        4: "pregnant cat",
-        5: "lactating cat (under 3 kittens)",
-        6: "lactating cat (3+ kittens)"
-    }
-    activity_labels = {
-        0: "neutered / inactive / indoor cats",
-        1: "active / outdoor cats"
-    }
-
-    kcal_by_activity = [
-        [52, 75],
-        [100, 100]
-    ]
-
-    age_modifier = [
-        [2, 2.5],
-        [1.75, 2],
-        [1.5, 1.5],
-        [1, 1],
-        [1.4, 1.4],
-        [1.2, 1.2],
-        [1.7, 1.7]
-    ]
-
+class Cat(object):
     def __init__(self, weight, age, activity):
+        self.activity = activity
+        self.kcal_needs_per_kg = nutritional_values.kcal_by_activity[self.activity]
         self.weight = weight
         self.age = age
-        self.activity = activity
-        self.age_modif = self.get_age_modifier()
+        self.age_modif = nutritional_values.age_modifier[self.age]
+
         # RER - Resting Energy Requirement (by weight only)
+        # '0.67' value taken from a nutritional guide. See nutritional_values.py for reference
         self.rer_formula_by_weight = self.weight * 0.67
+
         # DER - Daily Energy Requirement (by weight and activity)
         self.der = self.calculate_der()
-        self.der_avg = round(statistics.mean(self.der), 0)
+        self.der_avg = statistics.mean([self.der["min"], self.der["max"]])
+        self.der_avg = round(self.der_avg, 0)
+
         # MER - Metabolic Energy Requirement (total, by weight, activity and age)
         self.mer = self.calculate_mer()
-        self.mer_avg = round(statistics.mean(self.mer), 0)
+        self.mer_avg = statistics.mean([self.mer["min"], self.mer["max"]])
+        self.mer_avg = round(self.mer_avg, 0)
 
     def calculate_der(self):
-        return [round(self.rer_formula_by_weight * self.get_kcal_per_kg_by_activity()[0], 2),
-                round(self.rer_formula_by_weight * self.get_kcal_per_kg_by_activity()[1], 2)]
+        der = {}
 
-    def get_kcal_per_kg_by_activity(self):
-        kcal_per_kg_min = self.kcal_by_activity[self.activity][0]
-        kcal_per_kg_max = self.kcal_by_activity[self.activity][1]
-        return [kcal_per_kg_min, kcal_per_kg_max]
+        der["min"] = self.rer_formula_by_weight * self.kcal_needs_per_kg["min"]
+        der["min"] = round(der["min"], 2)
 
-    def get_age_modifier(self):
-        return self.age_modifier[self.age]
+        der["max"] = self.rer_formula_by_weight * self.kcal_needs_per_kg["max"]
+        der["max"] = round(der["max"], 2)
+
+        return der
 
     def calculate_mer(self):
-        mer_min = self.der[0] * self.age_modif[0]
-        mer_max = self.der[1] * self.age_modif[1]
-        return [mer_min, mer_max]
+        mer = {}
+
+        mer["min"] = self.der["min"] * self.age_modif["min"]
+        mer["min"] = round(mer["min"], 2)
+        mer["max"] = self.der["max"] * self.age_modif["max"]
+        mer["max"] = round(mer["max"], 2)
+
+        return mer
--- CatFoodCalc/test/test_cat.py
+++ CatFoodCalc/test/test_cat.py
@@ -3,34 +3,34 @@ import CatFoodCalc.cat_calc as cat_calc
 
 
 class TestCatCalculations(unittest.TestCase):
-    # weight, age, activity
-    data = [[4, 3, 0], [6, 4, 1], [2, 0, 1]]
-    cats_data = {}
-    # kcal per kg min, kcal per kg max, der min, der max, mer min, mer max
-    results = [[52, 75, 139.36, 201, 139.36, 201], [100, 100, 402, 402, 562.8, 562.8], [100, 100, 134, 134, 268, 335]]
-    cats_results = {}
+    test_cats_data = {
+        "cat1": [4, "adult", "indoor"],
+        "cat2": [6, "pregnant", "outdoor"],
+        "cat3": [2, "kitten1", "outdoor"]
+    }
+    test_cats_results = {
+        "cat1": {"der": {"min": 139.36, "max": 201}, "mer": {"min": 139.36, "max": 201}},
+        "cat2": {"der": {"min": 402, "max": 402}, "mer": {"min": 562.8, "max": 562.8}},
+        "cat3": {"der": {"min": 134, "max": 134}, "mer": {"min": 268, "max": 335}},
+    }
 
-    methods_to_test = ["cat_calc.Cat.get_kcal_per_kg_by_activity(tested_cat)[0]",
-                       "cat_calc.Cat.get_kcal_per_kg_by_activity(tested_cat)[1]",
-                       "cat_calc.Cat.calculate_der(tested_cat)[0]",
-                       "cat_calc.Cat.calculate_der(tested_cat)[1]",
-                       "cat_calc.Cat.calculate_mer(tested_cat)[0]",
-                       "cat_calc.Cat.calculate_mer(tested_cat)[1]"]
+    methods_to_test = {"der": "calculate_der",
+                       "mer": "calculate_mer"}
 
-    def construct_cats_with_results(self):
-        for x in range(len(self.data)):
-            self.cats_data[x] = self.data[x]
-            self.cats_results[x] = self.results[x]
+    def get_test_results(self, method, used_cat):
+        tested_method = "cat_calc.Cat." + str(method) + "(used_cat)"
+        result = eval(tested_method)
+        return result
 
     def method_base_test(self, methods):
-        for tested_function in methods:
-            for c, v in self.cats_data.items():
-                tested_cat = cat_calc.Cat(v[0], v[1], v[2])
-                expected = self.cats_results.get(c)[methods.index(tested_function)]
-                unittest.TestCase().assertEqual(eval(tested_function), expected)
+        for name, function in methods.items():
+            for cat, value in self.test_cats_data.items():
+                tested_cat = cat_calc.Cat(value[0], value[1], value[2])
+                expected = self.test_cats_results[cat][name]
+                actual = self.get_test_results(function, tested_cat)
+                unittest.TestCase().assertEqual(actual, expected)
 
     def testMethods(self, methods=methods_to_test):
-        self.construct_cats_with_results()
         self.method_base_test(methods)
 
 
